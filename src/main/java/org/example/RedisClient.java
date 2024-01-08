de59a0636346a9200a0233689666809085d3249d
hans.arias <hansarias74@gmail.com>
2024-01-08 16:50:53 -0500

    add loop to server and client, for sending and receiving different commands like ping and quit

--- src/main/java/org/example/RedisClient.java
+++ src/main/java/org/example/RedisClient.java
@@ -5,27 +5,45 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.net.Socket;
+import java.util.Optional;
 import java.util.Scanner;
 
 public class RedisClient {
 
     public static void main(String[] args) throws IOException {
         Socket socket = new Socket();
-        socket.connect(new InetSocketAddress("127.0.0.1", 6379), 1000);
+
+        try {
+            socket.connect(new InetSocketAddress("127.0.0.1", 6379), 1000);
+        } catch (Exception ex) {
+            System.out.println("Cannot connect to server redis " + ex.getMessage());
+            System.exit(-1);
+        }
 
         int port = socket.getPort();
         String host = socket.getLocalAddress().getHostAddress();
-
-        System.out.print(host + ":" + port + "> ");
 
         DataInputStream dataIn = new DataInputStream(socket.getInputStream());
         DataOutputStream dataOut = new DataOutputStream(socket.getOutputStream());
 
         Scanner sc = new Scanner(System.in);
 
-        String command = sc.nextLine();
-        dataOut.writeUTF(command);
-        System.out.print(dataIn.readUTF());
+        while (true) {
+            try {
+                System.out.print(host + ":" + port + "> ");
+
+                String command = sc.nextLine();
+                dataOut.writeUTF(command);
+
+                System.out.println(dataIn.readUTF());
+
+                if (command.toUpperCase().equals("QUIT")) break;
+            } catch (Exception exception) {
+                System.out.println(exception.getMessage());
+                break;
+            }
+
+        }
 
         dataIn.close();
         dataOut.close();
--- src/main/java/org/example/RedisServer.java
+++ src/main/java/org/example/RedisServer.java
@@ -5,22 +5,31 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.HashMap;
 
 public class RedisServer {
+
     public static void main(String[] args) throws IOException {
 
         ServerSocket serverSocket = new ServerSocket(6379);
+
+        System.out.println("Ready for accepting connections!");
 
         Socket clientSocket = serverSocket.accept();
 
         DataInputStream dataIn = new DataInputStream(clientSocket.getInputStream());
         DataOutputStream dataOut = new DataOutputStream(clientSocket.getOutputStream());
 
-        String command = dataIn.readUTF();
-        if (command.toUpperCase().equals("PING")) {
-            dataOut.writeUTF("PONG");
-        } else {
-            dataOut.writeUTF("(error) ERR unknown command " + command + ", with args beginning with:");
+        while (true) {
+            String command = dataIn.readUTF().toUpperCase();
+            System.out.println("command received " + command);
+            if (command.equals("PING")) {
+                dataOut.writeUTF("PONG");
+            } else if(command.toUpperCase().equals("QUIT")) {
+               break;
+            } else {
+                dataOut.writeUTF("(error) ERR unknown command " + command + ", with args beginning with:");
+            }
         }
 
         dataIn.close();
