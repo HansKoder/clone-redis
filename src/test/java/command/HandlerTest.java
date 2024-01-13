c4df25df2a78adf9fa5943dfa5408e169ea25cbb
hans.arias <hansarias74@gmail.com>
2024-01-12 21:13:13 -0500

    add two tests

--- src/test/java/command/HandlerTest.java
+++ src/test/java/command/HandlerTest.java
@@ -1,10 +1,17 @@
 package command;
 
 import org.example.command.Handler;
+import org.example.command.SetCommand;
+import org.example.store.Cache;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.MockedStatic;
+import org.mockito.Mockito;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.times;
 
 public class HandlerTest {
 
@@ -40,4 +47,33 @@ public class HandlerTest {
         assertFalse(handler.isStop());
     }
 
+    @Test
+    public void shouldSaveNewItemSuccessful () {
+        Cache cache = mock(Cache.class);
+
+        try (MockedStatic<Cache> cacheMockedStatic = mockStatic(Cache.class)) {
+            cacheMockedStatic.when(Cache::getInstance).thenReturn(cache);
+
+            doNothing().when(cache).set(anyString(), anyString());
+
+            assertEquals("OK", handler.process("set nickname example"));
+
+            Mockito.verify(cache, times(1)).set(anyString(), anyString());
+        }
+    }
+
+    @Test
+    public void shouldGetItemStored () {
+        Cache cache = mock(Cache.class);
+
+        try (MockedStatic<Cache> cacheMockedStatic = mockStatic(Cache.class)) {
+            cacheMockedStatic.when(Cache::getInstance).thenReturn(cache);
+
+            when(cache.get(anyString())).thenReturn("example-name");
+
+            assertEquals("example-name", handler.process("get name"));
+
+            Mockito.verify(cache, times(1)).get(anyString());
+        }
+    }
 }
